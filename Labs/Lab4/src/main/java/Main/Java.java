1fcfe24a56ebfdbd8d34840394d33fc7a219b3e1
Stefan <7stefanadrian@gmail.com>
2023-03-20 17:59:13 +0200

    Lab4 homework final version

--- Labs/Lab4/src/main/java/Main/Java.java
+++ Labs/Lab4/src/main/java/Main/Java.java
@@ -1,44 +1,100 @@
 package Main;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import com.github.javafaker.Faker;
+
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Java
 {
     public static void main(String[] args)
     {
+        //--------------------COMPULSORY PART-------
+
 
         //create a list of students
-        List<Student> stud_list = new LinkedList<Student>();
+        List<Student> stud_list = new LinkedList<>();
 
-        //use streams to create 10 students
-        IntStream.range(0, 10).forEach(i -> stud_list.add(new Student("Student " + i)));
+        Faker[] fake_stud = new Faker[10];
+
+        //[OPTIONAL] generate 30 fake project name
+        //Faker[] fake_proj = new Faker[30];
+
+        //generate 10 fakers
+        IntStream.range(0, 10).forEach(i -> fake_stud[i] = new Faker());
+
+        //IntStream.range(0, 10).forEach(i -> fake_proj[i] = new Faker());
+
+
+        //use streams to create 10 students and assign them random names
+        IntStream.range(0, 10).forEach(i -> stud_list.add(new Student(fake_stud[i].name().fullName())));
 
         //create a set of projects
-        Set<Project> proj_set = new TreeSet<Project>(Project::compareTo);
+        Set<Project> proj_set = new TreeSet<>(Project::compareTo);
+
+        Set<Project> proj_set_copy = new TreeSet<>(Project::compareTo);
 
 
         //use streams to create 20 projects
-        IntStream.range(0,20).forEach(i -> proj_set.add(new Project("Project "+i)));
+        IntStream.range(0, 10).forEach(i -> proj_set.add(new Project("P" + i)));
+        proj_set_copy.addAll(proj_set);
 
 
         //sort the students
-        stud_list.sort((s1, s2) -> s1.compareTo(s2));
+        // stud_list.sort((s1, s2) -> s1.compareTo(s2));
 
         //print the students and projects
 
+//        for (Student student : stud_list)
+//        {
+//            System.out.println(student);
+//        }
+//
+//        for (Project project : proj_set)
+//        {
+//            System.out.println(project);
+//        }
+
+
+        // ------------- HOMEWORK PART ---------------------
+
+        //create a problem instance
+        ProblemInstance problemInstance = new ProblemInstance(stud_list);
+
+        //assign a random project to each student once
+
         for (Student student : stud_list)
         {
-            System.out.println(student);
+            //get a random project
+            //get a random number -->  how many projects a student will have in preferences
+            int rand_number = new Random().nextInt(proj_set_copy.size() - 1);
+
+            for (int i = 0; i < rand_number; i++)
+            {
+                Project random_project = (Project) proj_set_copy.toArray()[(int) (Math.random() * proj_set_copy.size())];
+                //add the preference to the problem instance
+                problemInstance.addPreference(student, random_project);
+                //remove the project from the set
+                proj_set_copy.remove(random_project);
+            }
+
         }
 
-        for (Project project : proj_set)
-        {
-            System.out.println(project);
-        }
+        problemInstance.PrintPreferences();
+
+        //-----print the students that have a number of preferences lower than the average number of preferences---
+
+        // problemInstance.PrintAverage();
+
+        //solve the problem instance using a greedy algorithm
+        System.out.println();
+
+        problemInstance.solveGreedy(proj_set);
+        System.out.println("Solved using greedy algorithm : ");
+
+        problemInstance.PrintAssignments();
+
+
     }
 
 }
---
+++ Labs/Lab4/src/main/java/Main/ProblemInstance.java
@@ -0,0 +1,173 @@
+package Main;
+
+import java.util.*;
+import java.util.concurrent.RunnableFuture;
+
+/**
+ * A class that describres a problem instance for the project allocation problem
+ */
+public class ProblemInstance
+{
+
+    // Map that stores the preferences of each student
+    /**
+     * The preferences of each student
+     * Key is the student
+     * Value is the list of projects
+     */
+    private Map<Student, LinkedList<Project>> preferences = new HashMap<Student, LinkedList<Project>>();
+
+    /**
+     * The assignments of each student
+     */
+    private Map<Student, Project> assignments = new HashMap<Student, Project>();
+
+    public ProblemInstance(List<Student> student_list)
+    {
+        //initialize the preferences map
+
+        for (Student student : student_list)
+        {
+            preferences.put(student, new LinkedList<Project>());
+        }
+
+    }
+
+    /**
+     * Add a preference to the map
+     *
+     * @param s Student that has the preference
+     * @param p Project that is preferred
+     */
+    public void addPreference(Student s, Project p)
+    {
+        //get the list of projects for the student
+        LinkedList<Project> list = preferences.get(s);
+
+        //if the list is null, create a new list
+        list = list == null ? new LinkedList<Project>() : list;
+
+        //add the project to the list
+        if (!list.contains(p))
+        {
+            list.add(p);
+        } else
+        {
+            System.out.println("Project already exists in pteferences of the student" + s.toString());
+            return;
+        }
+
+        //update the map
+        this.preferences.put(s, list);
+
+
+    }
+
+    /**
+     * Get the preferences of a student
+     *
+     * @param s Student
+     * @return LinkedList of projects
+     */
+    public LinkedList<Project> getPreferences(Student s)
+    {
+        return preferences.get(s);
+    }
+
+    /**
+     * Print the preferences of each student
+     */
+    public void PrintPreferences()
+    {
+        preferences.entrySet().stream().forEach(e -> System.out.println(e.getKey().toString() + " " + e.getValue().toString()));
+    }
+
+    /**
+     * Print the students that have a number of preferences lower than the average number of preferences
+     */
+    public void PrintAverage()
+    {
+        //get the average number of preferences
+        double average = preferences.values().stream().mapToInt(LinkedList::size).average().getAsDouble();
+
+
+        //print the students that have a number of preferences lower than the average number of preferences
+        preferences.entrySet().stream().filter(e -> e.getValue().size() < average).forEach(e -> System.out.println(e.getKey().toString()));
+
+    }
+
+
+    /**
+     * Algorithm that solves the problem instance using a greedy algorithm
+     *
+     * @param projects projects that are available
+     */
+    public void solveGreedy(Set<Project> projects)
+    {
+        //list of projects that are not assigned
+
+        LinkedList<Project> unassigned_projects = new LinkedList<Project>();
+
+        unassigned_projects.addAll(List.copyOf(projects));
+
+        //list of students that have no project assigned
+        LinkedList<Student> unassigned_students = new LinkedList<Student>();
+
+        unassigned_students.addAll(List.copyOf(preferences.keySet()));
+
+
+        //sort the list of students by the number of preferences descending
+
+        unassigned_students.sort(Comparator.comparingInt(o -> preferences.get(o).size()).reversed());
+
+
+
+        //loop through the list of  students that have no project assigned
+        for (Student student : unassigned_students)
+        {
+            //get the list of preferences for the student
+            LinkedList<Project> preferences = getPreferences(student);
+
+            //for each project in the list of preferences
+            if (preferences.size() > 0)
+            {
+
+                //for each project in the list of preferences
+                for (Project project : preferences)
+                {
+                    //if the project is not assigned
+                    if (!assignments.containsValue(project))
+                    {
+                        //assign the project to the student
+                        assignments.put(student, project);
+                        //remove the project from the list of unassigned projects
+                        unassigned_projects.remove(project);
+                        break;
+                    }
+                }
+            }
+            else
+            {   //if the student has no preferences
+
+                // System.out.println("Student "+   student.toString() +" has no preferences .. ");
+
+                //assign the first unnassigned project to the student
+                assignments.put(student, unassigned_projects.getFirst());
+                //remove the project from the list of unassigned projects
+                unassigned_projects.remove(unassigned_projects.getFirst());
+
+            }
+        }
+
+
+        //for each student assign the first project from the list of preferences that is not assigned
+        //if all the projects are assigned, assign the first project from the list of preferences that is assigned to the student with the lowest number of preferences
+
+
+    }
+
+    public void PrintAssignments()
+    {
+        assignments.entrySet().stream().forEach(e -> System.out.println(e.getKey().toString() + " " + e.getValue().toString()));
+    }
+}
