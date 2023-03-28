850559990d5c7bed6c186845eae51c6b260726e1
Stefan <7stefanadrian@gmail.com>
2023-03-28 16:33:42 +0300

    Lab 6 compulsatory part

---
+++ Labs/Lab6-GIU/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class Main
+{
+    private static JPanel panel1;
+
+    public static void main(String[] args)
+    {
+            MainFrame frame = new MainFrame();
+    }
+}
